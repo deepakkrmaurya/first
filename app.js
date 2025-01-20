9c77887cf4b0335e348dd70bfc26fe01a0492d1a
deepakkrmaurya <deepakmaurya2211@gmail.com>
2025-01-20 17:30:38 +0530

    cors

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 import express from 'express'
+import cors from 'cors'
 const app = express();
 import dotenv from 'dotenv'
 dotenv.config();
 const base = '/api/v1'
 app.use(express.json());
+app.use(cors());
 import userRegister from './routes/user.route.js'
 app.use(`${base}/user`,userRegister);
 export default app
